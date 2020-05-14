c289c5eac91f54b728181b274f641529424532e4
chrisroselli <chrisroselli@gmail.com>
2020-05-14 15:53:41 -0400

    update luggers link

--- src/components/output-signature.vue
+++ src/components/output-signature.vue
@@ -5,7 +5,7 @@
         "background: none; border-width: 0px; border: 0px; margin: 0; padding: 0;">
         <tr>
             <td valign="top" style="padding-top: 0; padding-bottom: 0; padding-left: 0; padding-right: 7px; border-top: 0; border-bottom: 0; border-left: 0;">
-              <a href="https://www.junkluggers.com/">
+              <a href="https://www.luggers.com/">
                 <img v-show="mobilePhoneSwitch" width="108" height="139" style="width: 108px; max-width: 108px !important; height: 139px; max-height: 139px !important; margin-top: 5px;" id="preview-image-url" src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/2714/junkluggers-output-logos-three-cropped.jpg" name="preview-image-url" />
         
                 <img v-show="!mobilePhoneSwitch" width="88" height="113" style="width: 88px; max-width: 88px !important; height: 113px; max-height: 113px !important; margin-top: 5px;" id="preview-image-url" src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/2714/junkluggers-output-logos-three-cropped.jpg" name="preview-image-url" /> 
@@ -70,7 +70,7 @@
                   </td>
                   <td valign="top" style=
                   "vertical-align: top; color: #333333; font-size: 14px; font-family: Arial, Helvetica, sans-serif;">
-                  <a href="https://www.junkluggers.com/" target="_blank" style=
+                  <a href="https://www.luggers.com/" target="_blank" style=
                   "color: #333333; text-decoration: none; font-weight: normal; font-size: 14px;">
                     junkluggers.com</a>
                     &nbsp;&nbsp;
